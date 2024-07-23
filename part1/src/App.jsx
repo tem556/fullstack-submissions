f463589504915f24849e6e1b564ea2426e1e8d80
tem556 <tem556@github.com>
2024-07-23 19:22:54 +0300

    Add files to part0 and create part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,24 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+export default App
